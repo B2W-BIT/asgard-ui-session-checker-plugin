65b578a2c46ae934d86e942365057f8063f0fe39
Dalton Barreto <daltonmatos@gmail.com>
2018-01-30 10:05:25 -0200

    Registrando PRE_AJAX_REQUEST, se possível.
    Caso a funcionalidade de request pipeline estiver disponível registramos
    um callback para poder adicionar às requisições o token JWT.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 const {
   PluginHelper,
   PluginMountPoints,
+  PipelineNames,
+  PipelineStore,
 } = window.marathonPluginInterface;
 
 import SessionCheckerAction from "./actions/SessionCheckerAction";
@@ -13,3 +15,20 @@ PluginHelper.injectComponent(
   ChangeAccountComponent,
   PluginMountPoints.NAVBAR_TOP_RIGHT
 );
+
+if (PipelineNames && PipelineStore) {
+  console.log("Registering PRE_AJAX_REQUEST callback");
+  PipelineStore.registerOperator(
+    PipelineNames.PRE_AJAX_REQUEST,
+    (data) => {
+      const currentHeaders = data.headers;
+
+      var token = localStorage.getItem("auth_token");
+      currentHeaders["Authorization"] = "JWT " + token;
+      return Object.assign({}, data, {headers: currentHeaders});
+    }
+  );
+} else {
+  console.log("Request Pipelines feature not found...");
+}
+
